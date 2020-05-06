cc8818041a4e1ae1f2c1a1f69a675445dbcb025b
Timophey <55289941+TimoHub01@users.noreply.github.com>
2020-05-06 19:10:08 +0300

    Update centrmed_mysql_create.sql
    fix sql

--- centrmed_mysql_create.sql
+++ centrmed_mysql_create.sql
@@ -1,122 +1,115 @@
-CREATE TABLE `Запись` (
+CREATE TABLE IF NOT EXISTS `zapis` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Familia` tinytext NOT NULL,
+  `Imya` tinytext NOT NULL,
+  `Otchestvo` tinytext NOT NULL,
+  `Telefon` tinytext NOT NULL,
+  `Napravlenie` tinytext NOT NULL,
+  `Usluga` int(10) NOT NULL,
+  `Vrach` int(10) NOT NULL,
+  `Data` tinytext NOT NULL,
+  `Vremya` tinytext NOT NULL,
+  `Adress_Kliniki` tinytext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE `call` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`Фамилия` TEXT(20),
-	`Имя` TEXT(20),
-	`Отчество` TEXT(20),
-	`Телефон` bigint(11),
-	`Направление` longtext(150),
-	`Услуга` longtext(150),
-	`Врач` longtext(150),
-	`Дата` DATE,
-	`Время` TIME,
-	`Адрес клиники` longtext,
+	`fio` TEXT(20) NOT NULL,
+	`number` bigint(11) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Быстрая консультация` (
+CREATE TABLE `services` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`Фамилия` TEXT(20) NOT NULL,
-	`Имя` TEXT(20) NOT NULL,
-	`Отчество` TEXT(20) NOT NULL,
-	`email` longtext(150) NOT NULL,
-	`Телефон` bigint(11) NOT NULL,
-	`Направление` longtext(150) NOT NULL DEFAULT 'Терапия',
-	`Комментарий` longtext(1000) NOT NULL DEFAULT 'Терапия',
+	`specialization` longtext(150) NOT NULL,
+	`servuce` longtext(150),
+	`price` int(10) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Направления/услуги` (
+CREATE TABLE IF NOT EXISTS `vrachi` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Familia` tinytext NOT NULL,
+  `Imya` tinytext NOT NULL,
+  `Otch` tinytext NOT NULL,
+  `Stag` int(11) NOT NULL,
+  `Special` text NOT NULL,
+  `Uslugi` text NOT NULL,
+  `Relagii` text NOT NULL,
+  `RabDni` text NOT NULL,
+  `RabTime` text NOT NULL,
+  `Clinica` text NOT NULL,
+  `Cabinet` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+CREATE TABLE `clinics` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`Направление` longtext(150) NOT NULL,
-	`Услуга` longtext(150) NOT NULL,
-	`Цена` int(10) NOT NULL,
+	`name` longtext(150) NOT NULL,
+	`city` longtext(150) NOT NULL,
+	`street` longtext(150) NOT NULL,
+	`house` TEXT(20) NOT NULL,
+	`specializations` longtext(1500) NOT NULL,
+	`services` longtext(3000) NOT NULL,
+	`doctors` longtext(200) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Врачи` (
-	`id` int NOT NULL AUTO_INCREMENT,
-	`Фамилия` TEXT(20) NOT NULL,
-	`Имя` TEXT(20) NOT NULL,
-	`Отчество` TEXT(20) NOT NULL,
-	`Стаж` int NOT NULL,
-	`Специализация` longtext(150) NOT NULL,
-	`Услуги` longtext(150) NOT NULL,
-	`Регалии` longtext(300) NOT NULL,
-	`Рабочии дни на месяц` TEXT(20) NOT NULL,
-	`Рабочие время` TIME(20) NOT NULL,
-	`Клиника` longtext(150) NOT NULL,
-	`Кабинет` int NOT NULL,
-	PRIMARY KEY (`id`)
-);
+CREATE TABLE IF NOT EXISTS `svobodnoevremya` (
+  `id` int(11) NOT NULL,
+  `idvrach` int(11) NOT NULL,
+  `iddata` int(11) NOT NULL,
+  `Data` text NOT NULL,
+  `vrem1` text NOT NULL,
+  `vrem2` text NOT NULL,
+  `vrem3` text NOT NULL,
+  `vrem4` text NOT NULL,
+  `vrem5` text NOT NULL,
+  `vrem6` text NOT NULL,
+  `vrem7` text NOT NULL,
+  `vrem8` text NOT NULL,
+  `vrem9` text NOT NULL,
+  `vrem10` text NOT NULL,
+  `vrem12` text NOT NULL,
+  `vrem13` text NOT NULL,
+  `vrem14` text NOT NULL,
+  `vrem15` text NOT NULL,
+  `vrem16` text NOT NULL,
+  `vrem17` text NOT NULL,
+  `vrem18` text NOT NULL,
+  `vrem19` text NOT NULL,
+  `vrem20` text NOT NULL,
+  `vrem21` text NOT NULL,
+  `vrem22` text NOT NULL,
+  `vrem23` text NOT NULL,
+  `vrem24` text NOT NULL,
+  `vrem25` text NOT NULL,
+  `vrem26` text NOT NULL,
+  `vrem27` text NOT NULL,
+  `vrem28` text NOT NULL,
+  `vrem29` text NOT NULL,
+  `vrem30` text NOT NULL,
+  `vrem31` text NOT NULL,
+  `vrem32` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `Клиники` (
-	`id` int NOT NULL AUTO_INCREMENT,
-	`Название` longtext(150) NOT NULL,
-	`Город` longtext(150) NOT NULL,
-	`Улица` longtext(150) NOT NULL,
-	`Дом` TEXT(20) NOT NULL,
-	`Направления` longtext(1500) NOT NULL,
-	`Услуги` longtext(3000) NOT NULL,
-	`Кабинеты` longtext(200) NOT NULL,
-	`Врачи` longtext(200) NOT NULL,
-	PRIMARY KEY (`id`)
-);
+ALTER TABLE `Запись` ADD CONSTRAINT `Запись_fk0` FOREIGN KEY (`specialization`) REFERENCES `services`(`specialization`);
 
-CREATE TABLE `Свободное время для записи.` (
-	`id` bigint NOT NULL AUTO_INCREMENT,
-	`id Врача` bigint NOT NULL,
-	`Дата` DATE NOT NULL,
-	`Время1` TIME NOT NULL,
-	`Время2` TIME NOT NULL,
-	`Время3` TIME NOT NULL,
-	`Время4` TIME NOT NULL,
-	`Время5` TIME NOT NULL AUTO_INCREMENT,
-	`Время6` TIME NOT NULL AUTO_INCREMENT,
-	`Время32` TIME NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY (`id`)
-);
+ALTER TABLE `Запись` ADD CONSTRAINT `Запись_fk1` FOREIGN KEY (`service`) REFERENCES `services`(`servuce`);
 
-ALTER TABLE `Запись` ADD CONSTRAINT `Запись_fk0` FOREIGN KEY (`Направление`) REFERENCES `Направления/услуги`(`Направление`);
+ALTER TABLE `Запись` ADD CONSTRAINT `Запись_fk2` FOREIGN KEY (`doctor`) REFERENCES `Свободное время для записи.`(`id_doctor`);
 
-ALTER TABLE `Запись` ADD CONSTRAINT `Запись_fk1` FOREIGN KEY (`Услуга`) REFERENCES `Направления/услуги`(`Услуга`);
+ALTER TABLE `Запись` ADD CONSTRAINT `Запись_fk3` FOREIGN KEY (`date`) REFERENCES `Свободное время для записи.`(`date`);
 
-ALTER TABLE `Запись` ADD CONSTRAINT `Запись_fk2` FOREIGN KEY (`Врач`) REFERENCES `Свободное время для записи.`(`id Врача`);
+ALTER TABLE `Врачи` ADD CONSTRAINT `Врачи_fk0` FOREIGN KEY (`Специализация`) REFERENCES `services`(`specialization`);
 
-ALTER TABLE `Запись` ADD CONSTRAINT `Запись_fk3` FOREIGN KEY (`Дата`) REFERENCES `Свободное время для записи.`(`Дата`);
+ALTER TABLE `Врачи` ADD CONSTRAINT `Врачи_fk1` FOREIGN KEY (`Услуги`) REFERENCES `services`(`servuce`);
 
-ALTER TABLE `Запись` ADD CONSTRAINT `Запись_fk4` FOREIGN KEY (`Адрес клиники`) REFERENCES `Врачи`(`Клиника`);
+ALTER TABLE `Врачи` ADD CONSTRAINT `Врачи_fk2` FOREIGN KEY (`Клиника`) REFERENCES `clinics`(`id`);
 
-ALTER TABLE `Быстрая консультация` ADD CONSTRAINT `Быстрая консультация_fk0` FOREIGN KEY (`Направление`) REFERENCES `Направления/услуги`(`Направление`);
+ALTER TABLE `Свободное время для записи.` ADD CONSTRAINT `Свободное время для записи._fk0` FOREIGN KEY (`id_doctor`) REFERENCES `Врачи`(`id`);
 
-ALTER TABLE `Врачи` ADD CONSTRAINT `Врачи_fk0` FOREIGN KEY (`Специализация`) REFERENCES `Направления/услуги`(`Направление`);
-
-ALTER TABLE `Врачи` ADD CONSTRAINT `Врачи_fk1` FOREIGN KEY (`Услуги`) REFERENCES `Направления/услуги`(`Услуга`);
-
-ALTER TABLE `Врачи` ADD CONSTRAINT `Врачи_fk2` FOREIGN KEY (`Рабочии дни на месяц`) REFERENCES `Свободное время для записи.`(`Дата`);
-
-ALTER TABLE `Врачи` ADD CONSTRAINT `Врачи_fk3` FOREIGN KEY (`Клиника`) REFERENCES `Клиники`(`id`);
-
-ALTER TABLE `Клиники` ADD CONSTRAINT `Клиники_fk0` FOREIGN KEY (`Направления`) REFERENCES `Врачи`(`Специализация`);
-
-ALTER TABLE `Клиники` ADD CONSTRAINT `Клиники_fk1` FOREIGN KEY (`Услуги`) REFERENCES `Врачи`(`Услуги`);
-
-ALTER TABLE `Клиники` ADD CONSTRAINT `Клиники_fk2` FOREIGN KEY (`Врачи`) REFERENCES `Врачи`(`id`);
-
-ALTER TABLE `Свободное время для записи.` ADD CONSTRAINT `Свободное время для записи._fk0` FOREIGN KEY (`id Врача`) REFERENCES `Врачи`(`id`);
-
-ALTER TABLE `Свободное время для записи.` ADD CONSTRAINT `Свободное время для записи._fk1` FOREIGN KEY (`Дата`) REFERENCES `Врачи`(`Рабочии дни на месяц`);
-
-ALTER TABLE `Свободное время для записи.` ADD CONSTRAINT `Свободное время для записи._fk2` FOREIGN KEY (`Время1`) REFERENCES `Врачи`(`Рабочие время`);
-
-ALTER TABLE `Свободное время для записи.` ADD CONSTRAINT `Свободное время для записи._fk3` FOREIGN KEY (`Время2`) REFERENCES `Врачи`(`Рабочие время`);
-
-ALTER TABLE `Свободное время для записи.` ADD CONSTRAINT `Свободное время для записи._fk4` FOREIGN KEY (`Время3`) REFERENCES `Врачи`(`Рабочие время`);
-
-ALTER TABLE `Свободное время для записи.` ADD CONSTRAINT `Свободное время для записи._fk5` FOREIGN KEY (`Время4`) REFERENCES `Врачи`(`Рабочие время`);
-
-ALTER TABLE `Свободное время для записи.` ADD CONSTRAINT `Свободное время для записи._fk6` FOREIGN KEY (`Время5`) REFERENCES `Врачи`(`Рабочие время`);
-
-ALTER TABLE `Свободное время для записи.` ADD CONSTRAINT `Свободное время для записи._fk7` FOREIGN KEY (`Время6`) REFERENCES `Врачи`(`Рабочие время`);
-
-ALTER TABLE `Свободное время для записи.` ADD CONSTRAINT `Свободное время для записи._fk8` FOREIGN KEY (`Время32`) REFERENCES `Врачи`(`Рабочие время`);
+ALTER TABLE `Свободное время для записи.` ADD CONSTRAINT `Свободное время для записи._fk1` FOREIGN KEY (`date`) REFERENCES `Врачи`(`Рабочии дни на месяц`);
 
