833554881eddd27c2c427e70660606b8bf073af3
Dmitry <dmitry8.zaharov@mail.ru>
2020-05-09 16:01:47 +0300

    slider

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
    var i=1,count=0,d=0;
   function setimg(a){
-    if(a==0 && i==1) i=5;
+    if(a==0 && i==1) i=4;
     else if(a==0 && i>1) i--;
-    else if(a==1 && i<5) i++;
-    else if(a==1 && i==5) i=1;
-    if(i>2) var s="media/slide"+i+".jpg"
+    else if(a==1 && i<4) i++;
+    else if(a==1 && i==4) i=1;
+    if(i!=2) var s="media/slide"+i+".jpg"
     else var s="media/slide"+i+".png"
     document.getElementById("slide_id").src=s;
 
